11a1f4d4c2f5c5def4142c278a9b37c827276b9b
Mars-The-Planet <microsoft.maty@gmail.com>
2026-05-22 18:37:43 +0200

    backported to 1.18.2

--- common/src/main/java/com/mars/wishfulrecipes/mixin/TagLoaderMixin.java
+++ common/src/main/java/com/mars/wishfulrecipes/mixin/TagLoaderMixin.java
@@ -2,6 +2,7 @@ package com.mars.wishfulrecipes.mixin;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.tags.Tag;
 import net.minecraft.tags.TagLoader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,7 +11,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import static com.mars.wishfulrecipes.CommonClass.itemsInTags;
 
@@ -19,20 +23,20 @@ public class TagLoaderMixin<T> {
     @Shadow @Final private String directory;
 
     @Inject(method = "load", at = @At("RETURN"))
-    private void onRecipesLoaded(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, List<TagLoader.EntryWithSource>>> cir) {
-        Map<ResourceLocation, List<TagLoader.EntryWithSource>> loadedTags = cir.getReturnValue();
+    private void onRecipesLoaded(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, Tag.Builder>> cir) {
+        Map<ResourceLocation, Tag.Builder> loadedTags = cir.getReturnValue();
 
-        // accepting only item tags
-        if (!Objects.equals(this.directory, "tags/item")) return;
+        if (!Objects.equals(this.directory, "tags/items")) return;
 
-        loadedTags.forEach((tagId, entries) -> {
-            for (TagLoader.EntryWithSource entryWithSource : entries) {
+        loadedTags.forEach((tagId, builder) -> {
+            builder.getEntries().forEach(entryWithSource -> {
                 String item = entryWithSource.entry().toString();
                 String tag = tagId.toString();
 
                 // skipping nested tags
                 if (!item.startsWith("#")) {
                     List<String> items;
+                    // Assuming itemsInTags is defined elsewhere in your class
                     if (itemsInTags.containsKey(tag)) {
                         items = itemsInTags.get(tag);
                     }
@@ -42,7 +46,7 @@ public class TagLoaderMixin<T> {
                     items.add(item);
                     itemsInTags.put(tag, items);
                 }
-            }
+            });
         });
     }
 }
