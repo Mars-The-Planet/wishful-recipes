92e309ac90eca8c3639a93322d418e38bafa12ed
Mars-The-Planet <microsoft.maty@gmail.com>
2026-05-22 18:06:36 +0200

    backported to 1.20.1

--- common/src/main/java/com/mars/wishfulrecipes/mixin/PlayerListMixin.java
+++ common/src/main/java/com/mars/wishfulrecipes/mixin/PlayerListMixin.java
@@ -2,9 +2,8 @@ package com.mars.wishfulrecipes.mixin;
 
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
-import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.Recipe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,9 +17,9 @@ import static com.mars.wishfulrecipes.WishfulRecipesConfig.unlock_all_recipes;
 public abstract class PlayerListMixin {
     // unlock all recipes
     @Inject(method = "placeNewPlayer", at = @At("TAIL"))
-    private void placeNewPlayer(Connection connection, ServerPlayer player, CommonListenerCookie cookie, CallbackInfo ci) {
+    private void placeNewPlayer(Connection connection, ServerPlayer player, CallbackInfo ci) {
         if (!unlock_all_recipes) return;
-        Collection<RecipeHolder<?>> allRecipes = player.server.getRecipeManager().getRecipes();
+        Collection<Recipe<?>> allRecipes = player.server.getRecipeManager().getRecipes();
         player.awardRecipes(allRecipes);
     }
 }
