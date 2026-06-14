0727da2eb64940faba08ffefd788cdca23e8b0ce
Mars-The-Planet <microsoft.maty@gmail.com>
2026-06-14 13:26:17 +0200

    removed duplicate recipes

--- common/src/main/java/com/mars/wishfulrecipes/CommonClass.java
+++ common/src/main/java/com/mars/wishfulrecipes/CommonClass.java
@@ -1,6 +1,5 @@
 package com.mars.wishfulrecipes;
 
-import com.google.common.collect.Lists;
 import com.mars.deimos.config.DeimosConfig;
 import com.mars.deimos.datagen.DeimosRecipeGenerator;
 
@@ -12,6 +11,7 @@ import static com.mars.wishfulrecipes.WishfulRecipesConfig.*;
 
 public class CommonClass {
     public static Map<String, List<String>> itemsInTags = new HashMap<>();
+    public static Boolean alreadyGeneratedRecipes = false;
 
     public static void init() {
         DeimosConfig.init(Constants.MOD_ID, WishfulRecipesConfig.class);
@@ -49,24 +49,25 @@ public class CommonClass {
             }
         }
 
+        // Backported Recipes
         // 26.1 becomes official
-        if (backport_name_tag_enable)
-            DeimosRecipeGenerator.createShapelessRecipeJson(Lists.newArrayList("paper", "#c:nuggets"), "name_tag", 1);
+//        if (backport_name_tag_enable)
+//            DeimosRecipeGenerator.createShapelessRecipeJson(Lists.newArrayList("paper", "#c:nuggets"), "name_tag", 1);
 
         // 1.21.9 becomes official
         // added in 1.20.3, officially in 1.21
-        if (backport_copper_trapdoor_enable)
-            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("copper_ingot"), Lists.newArrayList("AA", "AA"), "copper_trapdoor", 2);
+//        if (backport_copper_trapdoor_enable)
+//            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("copper_ingot"), Lists.newArrayList("AA", "AA"), "copper_trapdoor", 2);
 
         // 1.21.6 becomes official
-        if (backport_saddle_enable)
-            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("leather", "iron_ingot"), Lists.newArrayList(" A ", "ABA"), "saddle", 1);
-        if (backport_lead_enable)
-            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("string"), Lists.newArrayList("AA ", "AA ", "  A"), "lead", 2);
+//        if (backport_saddle_enable)
+//            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("leather", "iron_ingot"), Lists.newArrayList(" A ", "ABA"), "saddle", 1);
+//        if (backport_lead_enable)
+//            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("string"), Lists.newArrayList("AA ", "AA ", "  A"), "lead", 2);
 
         // 1.21.5 becomes official
-        if (backport_lodestone_enable)
-            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("chiseled_stone_bricks", "iron_ingot"), Lists.newArrayList("AAA", "ABA", "AAA"), "lodestone", 1);
+//        if (backport_lodestone_enable)
+//            DeimosRecipeGenerator.createShapedRecipeJson(Lists.newArrayList("chiseled_stone_bricks", "iron_ingot"), Lists.newArrayList("AAA", "ABA", "AAA"), "lodestone", 1);
 
         // 1.21.2 becomes official
         // added in 1.17, made accessible using datapack in 1.19.3, officially in 1.21.2
